250042096786ca1c0b324d2a29b3642a0ec5585d
happycloudpak <happycloudpak@gmail.com>
2021-02-12 08:53:09 +0900

    add Resilience4jConfig

---
+++ src/main/java/com/springcloud/scg/Resilience4jConfig.java
@@ -0,0 +1,19 @@
+package com.springcloud.scg;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+
+@Configuration
+public class Resilience4jConfig {
+
+    @Bean
+    public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
+        ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
+        reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+        return reactiveResilience4JCircuitBreakerFactory;
+    }
+
+}

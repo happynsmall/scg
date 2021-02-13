9d89d76cb08de6dde8cbd2babcda2d0f0650fa68
happycloudpak <happycloudpak@gmail.com>
2021-02-13 13:25:06 +0900

    add

---
+++ src/main/java/com/springcloud/scg/Resilience4JConfig.java
@@ -0,0 +1,40 @@
+package com.springcloud.scg;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+import org.springframework.cloud.gateway.filter.factory.FallbackHeadersGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerResilience4JFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.DispatcherHandler;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+
+//@Configuration
+public class Resilience4JConfig {
+
+    //@Bean
+    public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
+        ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
+        reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+
+        reactiveResilience4JCircuitBreakerFactory.configure(
+                builder -> builder
+                        //.timeLimiterConfig(timeLimiterRegistry.getConfiguration("backendB").orElse(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(300)).build()))
+                        .circuitBreakerConfig(
+                            circuitBreakerRegistry
+                            .getConfiguration("mycb")
+                            .orElse(circuitBreakerRegistry.getDefaultConfig())
+                        )
+                ,
+                "mycb");
+
+        return reactiveResilience4JCircuitBreakerFactory;
+    }
+}
+

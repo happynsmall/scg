670208016df816b9df8b27165b1f8e06c37d7019
happycloudpak <happycloudpak@gmail.com>
2021-02-13 13:12:47 +0900

    a

--- src/main/java/com/springcloud/scg/ScgApplication.java
+++ src/main/java/com/springcloud/scg/ScgApplication.java
@@ -7,11 +7,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 public class ScgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScgApplication.class, args);
 	}
-	
+
 }

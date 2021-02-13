1c994312b6223c5d4bb74707f0031dc9c20205a0
happycloudpak <happycloudpak@gmail.com>
2021-02-13 13:07:26 +0900

    a

--- src/main/java/com/springcloud/scg/ScgApplication.java
+++ src/main/java/com/springcloud/scg/ScgApplication.java
@@ -2,12 +2,8 @@ package com.springcloud.scg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,23 +13,4 @@ public class ScgApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScgApplication.class, args);
 	}
-
-    @Bean
-    public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
-        ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
-        reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
-
-        reactiveResilience4JCircuitBreakerFactory.configure(
-				builder -> builder
-						//.timeLimiterConfig(timeLimiterRegistry.getConfiguration("backendB").orElse(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(300)).build()))
-						.circuitBreakerConfig(
-                            circuitBreakerRegistry
-                            .getConfiguration("myCB")
-                            .orElse(circuitBreakerRegistry.getDefaultConfig())
-                        )
-                ,
-				"myCB");
-
-        return reactiveResilience4JCircuitBreakerFactory;
-    }	
 }
